495208c193d5eacc83a07ec37f928ef04866c285
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2022-12-12 11:08:01 +0530

    Update Binary search with 2D array.cpp

--- Binary search with 2D array.cpp
+++ Binary search with 2D array.cpp
@@ -1,3 +1,4 @@
+// If the matrix is row wise sorted
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -18,6 +19,31 @@ public:
                 end = mid-1;
             }
             mid = start +(end-start)/2;
+        }
+        return 0;
+    }
+};
+
+// If the matrix Row and Col wise sorted
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int start = 0;
+        int end = col-1;
+        
+        while(start<row && end>=0){
+            int element = matrix[start][end];
+            if(element == target){
+                return 1;
+            }
+            if(element < target){
+                start++;
+            }
+            else{
+                end--;
+            }
         }
         return 0;
     }

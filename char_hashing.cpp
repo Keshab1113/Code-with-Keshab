60b46c327c642104a86e9369ec7849880add8cb6
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2023-07-30 01:31:25 +0530

    Update char_hashing.cpp

--- char_hashing.cpp
+++ char_hashing.cpp
@@ -18,3 +18,30 @@ int main(){
 // if te input is only lowercase then -a, for A then -A
 // And when all cheracter then only use hash[s[i]]++;
 // Our total char is 256
+
+
+//using unordered_map
+#include<iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    int s;
+    cin>>s;
+    int arr[s];
+    unordered_map<int, int>mpp;
+    //use unordered_map for best time complexity that is o(1)
+    for(int i=0;i<s;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp){
+        cout<< it.first<<"--> "<<it.second<<endl;
+    }
+    int k;
+    cin>>k;
+    while(k--){
+        int c;
+        cin>>c;
+        cout<<mpp[c]<<endl;
+    }
+}

b7fa501fbeccabc1e8938dc18067e88daa3065c1
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2023-07-30 01:08:22 +0530

    Update char_hashing.cpp

--- char_hashing.cpp
+++ char_hashing.cpp
@@ -15,3 +15,6 @@ int main(){
         cout<<hash[c-'a']<<endl;
     }
 }
+// if te input is only lowercase then -a, for A then -A
+// And when all cheracter then only use hash[s[i]]++;
+// Our total char is 256

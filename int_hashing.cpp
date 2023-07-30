5dc83589ac769eea7b39b3890c591086c9ceb42d
Keshab Das <110785343+Keshab1113@users.noreply.github.com>
2023-07-30 11:50:11 +0530

    Update int_hashing.cpp

--- int_hashing.cpp
+++ int_hashing.cpp
@@ -19,3 +19,31 @@ int main(){
         cout<<hash[c]<<endl;
     }
 }
+
+
+
+
+#include<iostream>
+#include <unordered_map>
+using namespace std;
+int main(){
+    int s;
+    cin>>s;
+    int arr[s];
+    unordered_map<int, int>mpp;
+    //use unordered_map for best time complexity that is o(1)
+    for(int i=0;i<s;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+    for(auto it:mpp){
+        cout<< it.first<<"--> "<<it.second<<endl;
+    }
+    int k;
+    cin>>k;
+    while(k--){
+        int c;
+        cin>>c;
+        cout<<mpp[c]<<endl;
+    }
+}
